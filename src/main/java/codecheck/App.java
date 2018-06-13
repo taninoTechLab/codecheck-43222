1c4da2773346517546ecbc072f3349cb815cc035
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-13 15:18:47 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -12,48 +12,5 @@ public class App {
 //		}
 	}
 
-    void runSample() {
-        Charset charset = StandardCharsets.UTF_8;
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet request = new HttpGet("http://httpbin.org/get");
-
-        System.out.println
-            ("requestの実行　「" + request.getRequestLine() + "」");
-            //requestの実行　「GET http://httpbin.org/get HTTP/1.1」
-
-        CloseableHttpResponse response = null;
-
-        try {
-            response = httpclient.execute(request);
-
-            int status = response.getStatusLine().getStatusCode();
-            System.out.println("HTTPステータス:" + status);
-            //HTTPステータス:200
-
-            if (status == HttpStatus.SC_OK){
-                String responseData =
-                    EntityUtils.toString(response.getEntity(),charset);
-                System.out.println(responseData);
-                //取得したデータが表示される
-            }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (response != null) {
-                    response.close();
-                }
-                if (httpclient != null) {
-                    httpclient.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }

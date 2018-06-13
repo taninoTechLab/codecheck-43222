39bbd543306271fbdf0eb8d0a83531cedf6c061b
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-13 14:46:39 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,5 +1,8 @@
 package codecheck;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
@@ -10,4 +13,29 @@ public class App {
 		println("test");
 		println("test2");
 	}
-}
+
+	@WebServlet("http://challenge-server.code-check.io/api/hash")
+	@SuppressWarnings("serial")
+	public class HashApi extends HttpServlet {
+		public void doGet(HttpServletRequest req, HttpServletResponse res)
+		    throws ServletException, IOException
+		  {
+		    List<String> hash = new List<String>;
+		    Res.json(
+		      res, Collections.singletonMap("q", hash)
+		    );
+		  }
+	}
+
+	public class Res {
+		  static Gson gson = new Gson();
+		  static void json(HttpServletResponse res, Object toJson)
+		    throws IOException
+		  {
+		    res.setContentType("application/json");
+		    res.setCharacterEncoding("utf-8");
+		    res.getWriter().println(
+		      gson.toJson(toJson)
+		    );
+		  }
+	}

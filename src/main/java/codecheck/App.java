7231bfc5a189c19c006dcdb5810f96d74d0f9f7f
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-18 12:00:15 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,16 +1,41 @@
 package codecheck;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class App {
 	public static void main(String[] args) {
 
 
 		System.out.println("test");
-//		runSample();
-//		for (int i = 0, l = args.length; i < l; i++) {
-//			String output = String.format("argv[%s]: %s", i, args[i]);
-//			System.out.println(output);
-//		}
+		String result = getResult("http://challenge-server.code-check.io/");
+		System.out.println(result);
+
+		for (int i = 0, l = args.length; i < l; i++) {
+			String output = String.format("argv[%s]: %s", i, args[i]);
+			System.out.println(output);
+		}
 	}
 
-
+	public static String getResult(String urlString){
+		String result = "";
+		try{
+		URL url = new URL(urlString);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.connect();
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+		con.getInputStream()));
+		String tmp = "";
+		while ((tmp = in.readLine()) != null) {
+		result += tmp;
+		}
+		in.close();
+		con.disconnect();
+		}catch(Exception e){
+		e.printStackTrace();
+		}
+		return result;
+		}
 }
